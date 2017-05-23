201beded4d593bd7c858471fe9cabf3d63d2d6f5
liqisheng <bruce.liqisheng66@gmail.com>
2017-05-23 13:49:12 +0800

    add index of test controller

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,15 @@ Rails.application.routes.draw do
   resources :orders do
     member do
       post :pay_with_alipay
-      post :pay_with_wechat 
+      post :pay_with_wechat
     end
   end
 
   namespace :account do
     resources :orders
   end
+
+  resources :tests
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
